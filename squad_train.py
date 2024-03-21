c5f8172d34a49a6b7051dab0d7600e036541ac93
andrewright17 <75227141+andrewright17@users.noreply.github.com>
2024-03-20 22:28:27 -0400

    squad_train commit

---
+++ squad_train.py
@@ -0,0 +1,20 @@
+from torch.utils.data import DataLoader
+from transformers import default_data_collator
+from datasets import load_dataset
+from transformers import AutoTokenizer, BertForQuestionAnswering
+from accelerate import Accelerator
+
+#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+accelerator = Accelerator(mixed_precision='no')
+
+#model_name = 'bert-base-uncased'
+
+# Load the tokenizer and model
+#tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+#model = BertForQuestionAnswering.from_pretrained(model_name)
+
+#if accelerator.device.type == "cuda":
+#    model.to(accelerator.device)
+
+print(accelerator.device)
